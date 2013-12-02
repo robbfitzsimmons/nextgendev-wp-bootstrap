279f3fa1147adba7a9b35d56949f8a15cf46409a
robbfitzsimmons <robb@proximate.com>
2013-12-02 13:22:02 -0600

    Adding Facebook button

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 			<footer role="contentinfo" >
-			
+
 				<div id="nextgen-footer-logo" class="pull-right hidden-phone">
 					<img src="<?php bloginfo('template_directory'); ?>/images/ngd_logo.png"/>
 				</div>
-				
+
 				<div id="inner-footer" class="clearfix">
 		          <div id="widget-footer" class="clearfix row-fluid">
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
@@ -13,28 +13,29 @@
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
 		            <?php endif; ?>
 		          </div>
-					
+
 					<nav class="clearfix">
 						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
-					
+
 					<p class="pull-right hidden-phone">
-						<a href="http://us.linkedin.com/company/next-generation-development-llc" id="linkedin"><img src="<?php bloginfo('template_directory'); ?>/images/linkedin.png" style="width: 35px"/></a>
+						<a href="https://www.facebook.com/NextGenerationDevelopment" id="facebook"><img src="<?php bloginfo('template_directory'); ?>/images/facebook.png" style="width: 35px"/></a>
+          	<a href="http://us.linkedin.com/company/next-generation-development-llc" id="linkedin"><img src="<?php bloginfo('template_directory'); ?>/images/linkedin.png" style="width: 35px"/></a>
           </p>
-			
+
 					<p class="attribution hidden-phone">&copy; 2012 Next Generation Development. All rights reserved.</p>
-				
+
 				</div> <!-- end #inner-footer -->
-				
+
 			</footer> <!-- end footer -->
-		
+
 		</div> <!-- end #container -->
-				
+
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 		<![endif]-->
-		
+
 		<?php wp_footer(); // js scripts are inserted using this function ?>
 
 	</body>
